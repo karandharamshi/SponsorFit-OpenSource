d11944e3137d27ad984b54eb802aca86c0589a98
Karan Dharamshi <karan.dharamshi@gmail.com>
2026-07-02 10:13:27 +0100

    Add safe form field rules

---
+++ packages/form_filler/safe_fields.py
@@ -0,0 +1,29 @@
+SAFE_FIELD_HINTS = {
+    "first_name": ["first name", "firstname", "given name"],
+    "last_name": ["last name", "lastname", "surname", "family name"],
+    "email": ["email", "e-mail"],
+    "phone": ["phone", "mobile", "telephone"],
+    "city": ["city", "town"],
+    "country": ["country"],
+}
+
+SENSITIVE_HINTS = [
+    "submit",
+    "send application",
+    "declaration",
+    "right to work",
+    "visa",
+    "sponsorship",
+    "disability",
+    "ethnicity",
+    "gender",
+    "religion",
+    "criminal",
+    "conviction",
+    "medical",
+    "reasonable adjustment",
+]
+
+
+def is_sensitive_field(label: str) -> bool:
+    return any(hint in label.lower() for hint in SENSITIVE_HINTS)
